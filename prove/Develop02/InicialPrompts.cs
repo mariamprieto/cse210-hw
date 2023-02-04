f43e8eaea4b59eb7622c12f80d2c5dcae65f3e15
mariamprieto <mariaprieto@byui.edu>
2023-02-04 18:28:30 -0400

    Loading

--- prove/Develop02/InicialPrompts.cs
+++ prove/Develop02/InicialPrompts.cs
@@ -122,14 +122,32 @@ public class initialPrompt
     public void LoadJournal()
     {
 
-        string filename = "myFile.txt";
-        string[] lines = System.IO.File.ReadAllLines(filename);
-
-        foreach (string line in lines)
+        string line;
+        try
         {
-            string[] parts = line.Split(",");
-
-          
+            //Pass the file path and file name to the StreamReader constructor
+            StreamReader sr = new StreamReader("C:\\Users\\user01\\Desktop\\cse210-hw\\prove\\Develop02\\myFile.txt");
+            //Read the first line of text
+            line = sr.ReadLine();
+            //Continue to read until you reach end of file
+            while (line != null)
+            {
+                //write the line to console window
+                Console.WriteLine(line);
+                //Read the next line
+                line = sr.ReadLine();
+            }
+            //close the file
+            sr.Close();
+            Console.ReadLine();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Executing finally block.");
         }
     }
 
