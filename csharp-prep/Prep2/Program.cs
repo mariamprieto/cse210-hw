d73f77fa4eb87d86a72168cf1ab6fafe9e8663b6
Maria Prieto <mariaprieto@byui.edu>
2025-04-26 00:10:58 +0200

    Range limitation from 0 to 100

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -7,45 +7,53 @@ class Program
         Console.Write("Please write your score percentage: ");
         String score = Console.ReadLine();
         int x = int.Parse(score);
-      
 
 
-           if (x >= 70)
+
+        if ((x >= 70) && (x <= 100))
         {
             Console.WriteLine("Congratulations! You've passed this course.");
-           
-           if (x >= 90)
-            { 
-                string letter= "A";
+
+            if ((x >= 90) && (x <= 100))
+            {
+                string letter = "A";
                 Console.WriteLine($"Your grade letter is {letter}.");
             }
 
-            if ((x >= 80) && (x<90) )
+            if ((x >= 80) && (x < 90))
             {
-                string letter= "B";
+                string letter = "B";
                 Console.WriteLine($"Your grade letter is {letter}.");
             }
-            if ((x >= 70) && (x<80) )
+            if ((x >= 70) && (x < 80))
             {
-                string letter= "C";
+                string letter = "C";
                 Console.WriteLine($"Your grade letter is {letter}.");
             }
+           
+        }
+
+        else if ((x >=0) && (x<70))
+        {
+            Console.WriteLine("Sorry,your score wasn't enough to pass this course, don't give up and keep trying");
+            if ((x >= 60) && (x < 70))
+            {
+                string letter = "D";
+                Console.WriteLine($"Your grade letter is {letter}.");
+            }
+            else if ((x>=0) && (x<60))
+            {
+                string letter = "F";
+                Console.WriteLine($"Your grade letter is {letter}.");
+            }
+
+            
         }
 
         else
-        {
-            Console.WriteLine("Sorry,your score wasn't enough to pass this course, don't give up and keep trying");
-            if (x >= 60 && (x<70))
             {
-                string letter= "D";
-                Console.WriteLine($"Your grade letter is {letter}.");
+                Console.WriteLine("Score incorrect, please try again");
             }
-            if (x < 60)
-            {
-                string letter= "F";
-                Console.WriteLine($"Your grade letter is {letter}.");
-            }
-        }
     }
 
 
