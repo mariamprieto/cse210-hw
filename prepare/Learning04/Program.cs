c4458d60e0e43c42165adcf85e6f1e01a9378c9e
Maria Prieto <mariaprieto@byui.edu>
2025-06-07 15:34:05 +0200

    Update project

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment homework1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(homework1.GetSummary());
+        
+        MathAssignment homework2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(homework2.GetSummary());
+        Console.WriteLine(homework2.GetHomeworkList());
+
+        WritingAssignment homework3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(homework3.GetSummary());
+        Console.WriteLine(homework3.GetWritingInformation());
+
     }
 }
