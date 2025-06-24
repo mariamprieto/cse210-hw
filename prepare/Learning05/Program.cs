a063c19e9e7109775f5a085e4cf75569db233fcc
Maria Prieto <mariaprieto@byui.edu>
2025-06-24 22:38:57 +0200

    update project

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.ComponentModel;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square shape01 = new Square("Red", 10);
+        Rectangle shape02 = new Rectangle("Green", 10, 25);
+        Circle shape03 = new Circle("Blue", 2.5);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(shape01);
+        shapes.Add(shape02);
+        shapes.Add(shape03);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
